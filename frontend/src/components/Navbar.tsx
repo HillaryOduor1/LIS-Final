c464654a912669534caf1985738fbf6e9867623b
Hillary <hillaryoduor0@gmail.com>
2026-06-02 06:00:26 +0300

    fixed content

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -53,12 +53,12 @@ export default function Navbar({ isSidebarOpen, setIsSidebarOpen }: NavbarProps)
             <MenuIcon width={22} height={22} />
           </button>
           <Link to="/" className="flex items-center gap-3" onClick={() => trackEvent('logo_click')}>
-            <div className="size-8 text-primary">
+            {/*<div className="size-8 text-primary">
               {/*<svg fill="none" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg">
                 <path d="M8.57829 8.57829C5.52816 11.6284 3.451 15.5145 2.60947 19.7452C1.76794 23.9758 2.19984 28.361 3.85056 32.3462C5.50128 36.3314 8.29667 39.7376 11.8832 42.134C15.4698 44.5305 19.6865 45.8096 24 45.8096C28.3135 45.8096 32.5302 44.5305 36.1168 42.134C39.7033 39.7375 42.4987 36.3314 44.1494 32.3462C45.8002 28.361 46.2321 23.9758 45.3905 19.7452C44.549 15.5145 42.4718 11.6284 39.4217 8.57829L24 24L8.57829 8.57829Z" fill="currentColor"></path>
-              </svg>*/}
-            </div>
-            <h1 className="text-xl font-extrabold tracking-tight text-[#0d1b14] dark:text-white">Landscapes Integrity SolutionsS</h1>
+              </svg>}
+            </div>*/}
+            <h1 className="text-xl font-extrabold tracking-tight text-[#0d1b14] dark:text-white">Landscapes Integrity Solutions</h1>
           </Link>
         </div>
 
